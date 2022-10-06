164d165c53b4547105f60717c6be9a49850d8a88
Anubhav Lal <67056859+anubhavlal07@users.noreply.github.com>
2022-10-06 23:47:38 +0530

    Updated api key

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const apiKey = "vNLw6hj1HAsjNquPZk2h-UVW8aTCUkLPsSkK9IVqMiM";
+const apiKey = "ec33565ea65549e8a9776e5cfcf85da7";
 let newsAccordion = document.getElementById("newsAccordion");
 let errorPage = document.getElementById("parent");
 var headlineCount = 40;
@@ -10,7 +10,7 @@ var requestOptions = {
   },
 };
 fetch(
-  `https://api.newscatcherapi.com/v2/latest_headlines?countries=in&lang=en&page_size=${headlineCount}`,
+  `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`,
   requestOptions
 )
   .then((response) => response.text())
@@ -21,9 +21,13 @@ fetch(
     let newsHtml = "";
     if (json.status != "error") {
       articles.forEach(function (element, index) {
-        if (element.twitter_account != null && element.media != null && element.summary != "") {
-          console.log(element, index);
-          let time = new Date(element.published_date).toLocaleTimeString();
+        if (
+          element.source.name != null &&
+          element.urlToImage != null &&
+          element.description != ""
+        ) {
+          // console.log(element, index);
+          let time = new Date(element.publishedAt).toLocaleTimeString();
           let news = `<div class="accordion-item">
       <h2 class="accordion-header" id="flush-heading${index}">
           <button class="accordion-button collapsed bg-info text-white" type="button" data-bs-toggle="collapse"
@@ -33,19 +37,18 @@ fetch(
       </h2>
       <div id="flush-collapse${index}" class="accordion-collapse collapse" aria-labelledby="flush-heading${index}"
           data-bs-parent="#newsAccordion">
-          <img src="${element.media}" class="img-fluid mx-auto d-block" alt="${element.title}">
+          <img src="${element.urlToImage}" class="img-fluid mx-auto d-block" alt="${element.source.name}">
           <div class="accordion-body">
-          <h6 class="card-subtitle mb-2 text-muted">${element.twitter_account} at ${time}</h6>
-          <div>${element.summary}</div>
-          <a href="${element.link}" target="_blank" class="card-link text-black">Read Entire Article</a></div>
+          <h6 class="card-subtitle mb-2 text-muted">${element.source.name} at ${time}</h6>
+          <div>${element.description}</div>
+          <a href="${element.url}" target="_blank" class="card-link text-black">Read Entire Article</a></div>
       </div>
        </div>`;
-       newsHtml += news;
+          newsHtml += news;
         }
       });
       newsAccordion.innerHTML = newsHtml;
-    }
-    else {
+    } else {
       let news = `<section class="centered">
       <h6 class="d-flex justify-content-center text-white" id="time"></h6>
       <h1>500 Server Error</h1>
@@ -67,37 +70,37 @@ setInterval(() => {
 
 // Dynamic year Footer
 let year = new Date().getFullYear();
-document.getElementById("footer").innerHTML = `Developed by <a href="https://github.com/anubhavlal07" target="_blank">Anubhav Lal</a> | &copy; ${year} All Rights Reserved.`;
+document.getElementById(
+  "footer"
+).innerHTML = `Developed by <a href="https://github.com/anubhavlal07" target="_blank">Anubhav Lal</a> | &copy; ${year} All Rights Reserved.`;
 
 // Diable input from users
-(document.onkeydown = function (event) {
-  if (event.keyCode == 123) {
-    return false;
-  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
-    return false;
-  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 67) {
-    return false;
-  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 86) {
-    return false;
-  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 117) {
-    return false;
-  } else if (event.ctrlKey && event.keyCode == 85) {
-    return false;
-  }
-}),
-  false;
-if (document.addEventListener) {
-  document.addEventListener(
-    "contextmenu",
-    function (e) {
-      e.preventDefault();
-    },
-    false
-  );
-} else {
-  document.attachEvent("oncontextmenu", function () {
-    window.event.returnValue = false;
-  });
-}
-
-
+// (document.onkeydown = function (event) {
+//   if (event.keyCode == 123) {
+//     return false;
+//   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+//     return false;
+//   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 67) {
+//     return false;
+//   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 86) {
+//     return false;
+//   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 117) {
+//     return false;
+//   } else if (event.ctrlKey && event.keyCode == 85) {
+//     return false;
+//   }
+// }),
+//   false;
+// if (document.addEventListener) {
+//   document.addEventListener(
+//     "contextmenu",
+//     function (e) {
+//       e.preventDefault();
+//     },
+//     false
+//   );
+// } else {
+//   document.attachEvent("oncontextmenu", function () {
+//     window.event.returnValue = false;
+//   });
+// }
